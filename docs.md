a0aa477c81334595a9f7856bfcc95271fccb500f
Antonio Cheong <acheong@student.dalat.org>
2023-02-10 00:07:12 +0800

    Remove old stuff

--- docs.md
+++
@@ -1,7 +0,0 @@
-# Sydney Project Documentation
-
-## Endpoints
-
-Chat: `wss://sydney.bing.com/sydney/ChatHub`
-
-Conversation creation: `https://www.bing.com/turing/conversation/create`
--- parse_cookies.py
+++
@@ -1,9 +0,0 @@
-cookies = {}
-with open("cookies.txt") as f:
-    cookies = f.read().split("; ")
-    # Everything after the first "=" is the value (there may be multiple =)
-    cookies = {
-        cookie.split("=")[0]: "=".join(cookie.split("=")[1:]) for cookie in cookies
-    }
-
-print(cookies)
