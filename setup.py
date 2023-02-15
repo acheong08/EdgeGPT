23a938c871e92f5c7cdaff2ba53e1a6436f005a8
Antonio Cheong <acheong@student.dalat.org>
2023-02-15 13:31:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="EdgeGPT",
-    version="0.0.30",
+    version="0.0.40",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
@@ -11,7 +11,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/acheong08/EdgeGPT",
-    install_requires=["asyncio", "requests", "websockets"],
+    install_requires=["asyncio", "tls-client", "websockets"],
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     py_modules=["EdgeGPT"],
