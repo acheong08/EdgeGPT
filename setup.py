e6b9355ba90b2143f6a7c5a75f5e870813716c99
Antonio Cheong <acheong@student.dalat.org>
2023-03-04 20:58:11 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="EdgeGPT",
-    version="0.0.53.1",
+    version="0.0.54",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
