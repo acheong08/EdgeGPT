ff34ac03a08010b03cbae3c7f1e7a3d0c2cca1f7
CoolPlayLin <coolplaylin@qq.com>
2023-03-11 10:24:36 +0800

    improve setup.py and add PyPi Package build checking (#119)

--- setup.py
+++ setup.py
@@ -11,8 +11,20 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/acheong08/EdgeGPT",
+    project_urls={
+    "Bug Report": "https://github.com/acheong08/EdgeGPT/issues/new"
+    },
     install_requires=["asyncio", "requests", "websockets"],
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     py_modules=["EdgeGPT"],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ]
 )
