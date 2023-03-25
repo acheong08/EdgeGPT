9dda42f536246c38dbad1da9326ba84f7113f232
Antonio Cheong <acheong@student.dalat.org>
2023-03-25 10:55:58 +0800

    auto retry with proxy

--- src/EdgeGPT.py
+++ src/EdgeGPT.py
@@ -219,10 +219,14 @@ class Conversation:
             or "https://edgeservices.bing.com/edgesvc/turing/conversation/create"
         )
         if response.status_code != 200:
-            print(f"Status code: {response.status_code}")
-            print(response.text)
-            print(response.url)
-            raise Exception("Authentication failed")
+            response = self.session.get(
+                "https://edge.churchless.tech/edgesvc/turing/conversation/create"
+            )
+            if response.status_code != 200:
+                print(f"Status code: {response.status_code}")
+                print(response.text)
+                print(response.url)
+                raise Exception("Authentication failed")
         try:
             self.struct = response.json()
             if self.struct["result"]["value"] == "UnauthorizedRequest":
