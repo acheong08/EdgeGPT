17190461d027654d2f4964cd0a6f129d9b8e018d
Antonio Cheong <acheong@churchless.tech>
2023-03-31 11:21:29 +0800

    minor bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="EdgeGPT",
-    version="0.1.13",
+    version="0.1.13.1",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
