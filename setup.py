6393298c363bb0da30ac7e1385b5f58ff934cba2
Antonio Cheong <acheong@churchless.tech>
2023-04-16 22:31:08 +0800

    0.1.25
    bugfix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if not PATH.exists():
 
 setup(
     name="EdgeGPT",
-    version="0.1.24",
+    version="0.1.25",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
