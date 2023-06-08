7ad034f20846f706c3e04d6e869fcd9f2678335e
Antonio Cheong <acheong@churchless.tech>
2023-06-08 16:26:43 +0800

    log response

--- test_base.py
+++ test_base.py
@@ -2,11 +2,9 @@ import pytest
 import asyncio
 from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 
-def test_base():
-    asyncio.run(test_ask())
-
 async def test_ask():
     bot = await Chatbot.create() # Passing cookies is "optional", as explained above
     response = await bot.ask(prompt="Hello world", conversation_style=ConversationStyle.creative)
     await bot.close()
+    print(response)
     assert response != ""
