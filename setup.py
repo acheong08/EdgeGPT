c2b9fe4995db85d16104c52470843c89d6f72075
Antonio <acheong@student.dalat.org>
2023-06-15 15:54:30 +0800

    0.10.15 (close #542)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if not PATH.exists():
 
 setup(
     name="EdgeGPT",
-    version="0.10.14",
+    version="0.10.15",
     license="GNU General Public License v2.0",
     author="Antonio Cheong",
     author_email="acheong@student.dalat.org",
