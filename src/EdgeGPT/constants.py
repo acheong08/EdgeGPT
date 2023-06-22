9b32a3292903575c8ea5c26ed86289d4ad9cfa4c
Antonio <acheong@student.dalat.org>
2023-06-22 12:04:58 +0800

    US residential IP spoof

--- src/EdgeGPT/constants.py
+++ src/EdgeGPT/constants.py
@@ -5,9 +5,7 @@ DELIMITER = "\x1e"
 
 
 # Generate random IP between range 13.104.0.0/14
-FORWARDED_IP = (
-    f"13.{random.randint(104, 107)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
-)
+FORWARDED_IP = f"1.0.0.{random.randint(0, 255)}"
 
 HEADERS = {
     "accept": "application/json",
